9b046790f71b31415cd0c64412ceafc117f7124a
amandalin047 <amanda.lin0103@gmail.com>
2026-05-03 06:53:03 +0800

    epoching pipeline seems successful

---
+++ utils/epoch_and_average.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+import mne
+from collections import OrderedDict
+from collections.abc import Iterable
+
+def revise_annot(df_annot: pd.DataFrame, *,
+                 fixation: str, non_final: str, item_codes: list|tuple,
+                 **conditions) -> mne.Annotations:   
+    """IMPORTANT!!!
+       df_annot: please get the data frame via pd.DataFrame(raw.annotations);
+                 do NOT use raw.annotations.to_data_frame() -- I don't know why this causes downstream annotations setting to be empty, i.e.,
+                 if annot_revised is the returned revised mne.Annotations object, then doing
+                 raw.set_annotations(annot_revised) will lead to empty annoations.
+       conditions should be: event_description=event_codes, where event_codes is an iterable, e.g.,
+       high_constraint=(240, 241, 242, 243), low_constraint=(244, 245, 246, 247)"""
+    
+    if "orig_time" not in df_annot.columns or "extras" not in df_annot.columns:
+        print(f"""ABORT: either 'orig_time' or 'extras' was not found in df_annot.columns, which is consistent with if the
+        annotations data frame was converted via mne.Annotations.to_data_frame(), which will lead to downstream errors.
+        Please ensure that df_annot is converted directly via Pandas DataFrame constructor, i.e.,
+        df_annot = Pandas.DataFrame(mne.Annotations).""")
+        return
+    
+    cond_dict = OrderedDict({key: [str(v) for v in val]
+                                          for key, val in conditions.items()}) # in case integer event codes are passed
+                                                                               # and ensures key (condition description) order
+    cond_descs = list(cond_dict.keys())
+    all_cond_codes = np.array(list(cond_dict.values()))
+    
+    desc = list(df_annot["description"])
+    desc = [d.strip() for d in desc]  # remove leading or trailing white spaces to be safe
+    desc_revised = desc.copy()
+
+    for i, d in enumerate(desc):
+        if d == fixation:
+            step = 1
+        elif d == non_final:
+            desc_revised[i] = "w" + str(step) 
+            step += 1
+        elif d in item_codes:
+            if desc[i-1] == fixation:
+                desc_revised[i] = "w" + str(step) 
+                step += 1
+            elif desc[i-1] == non_final:
+                desc_revised[i] = "w" + str(step) 
+        elif d in all_cond_codes.flatten():  # use flatten, as flatten always returns a copy; need all_cond_codes to stay 2D
+            cond_idx = np.where(all_cond_codes == d)[0][0]
+            cond = cond_descs[cond_idx]
+            desc_revised[i-step: i] = [s + f"/{cond}" for s in desc_revised[i-step: i]]
+            step = 0
+    
+    annot_revised = mne.Annotations(onset=df_annot["onset"], duration=df_annot["duration"], description=desc_revised)
+    return annot_revised
+
+def eeglab_bin_epoch(raw: mne.io.Raw,
+                     fixation: str,
+                     non_final: str,
+                     item_codes: list|tuple,
+                     position_range: tuple,
+                     conditions_list: list,
+                     tmin: float, tmax: float,
+                     baseline: tuple | None,
+                     resample: float | None=None,
+                     verbose: bool=False):
+    """condtitions_list should be a list of dicts, e.g.,
+       [{'high_constraint': (240, 241, 242, 243), 'low_constraint': (244, 245, 246, 247)},
+        {'emo_word': (240, 241, 244, 245), 'neu_word': (242, 243, 246, 247)}]"""
+    
+    def get_time_locks_descs(position_range: tuple,
+                             cond_descs: Iterable):
+        word_positions = [f"w{i}" for i in range(position_range[0], position_range[1]+1)]
+        time_locks_descs = [f"{w}/{c}" for w in word_positions for c in cond_descs]
+        return time_locks_descs
+    
+    df_annot = pd.DataFrame(raw.annotations)
+    
+    epochs_list = []
+    for conditions in conditions_list:
+        annot_revised = revise_annot(df_annot.copy(),
+                                 fixation=fixation,
+                                 non_final=non_final,
+                                 item_codes=item_codes,
+                                 **conditions)
+        raw_revised = raw.copy()
+        raw_revised.set_annotations(annot_revised)
+
+        events, event_id = mne.events_from_annotations(raw_revised)
+        time_locks_descs = get_time_locks_descs(position_range, conditions)
+        time_locks = {k: v for k, v in event_id.items() if k in time_locks_descs}
+
+        epochs = mne.Epochs(raw_revised,
+                            events=events, event_id=time_locks,
+                            tmin=tmin, tmax=tmax, baseline=baseline,  
+                            on_missing="raise",
+                            preload=True,
+                            verbose=verbose)
+        if resample is not None:
+            epochs = epochs.resample(sfreq=resample)
+        epochs_list.append(epochs)
+    return epochs_list
